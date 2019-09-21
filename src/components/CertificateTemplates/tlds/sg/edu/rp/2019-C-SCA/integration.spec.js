31088dfb6ff791b6e7555da781d89d843490a6e3
RP-OIS-SK <lim_swee_kiew@rp.edu.sg>
2019-09-22 00:31:58 +0800

    to add certificate template for RP short course

--- src/components/CertificateTemplates/tlds/sg/edu/rp/2019-C-SCA/integration.spec.js
+++ src/components/CertificateTemplates/tlds/sg/edu/rp/2019-C-SCA/integration.spec.js
@@ -2,7 +2,7 @@ import { Selector } from "testcafe";
 
 fixture("Republic Polytechnic").page`http://localhost:3000`;
 
-const Certificate = "./RP_SCA_15060278.opencert";
+const Certificate = "./RP_SCA.opencert";
 
 const TemplateTabList = Selector("#template-tabs-list");
 const RenderedCertificate = Selector("#rendered-certificate");
@@ -23,8 +23,9 @@ test("CET Short Course certificate is rendered correctly", async t => {
   // Certificate tab content
   await validateTextContent(t, RenderedCertificate, [
     "Student LIM",
-    "UIPath - GoRPA",
+    "UIPath",
     "Director ACE",
     "Republic Polytechnic"
   ]);
 });
+
